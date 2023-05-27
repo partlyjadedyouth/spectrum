5a0c2fa34ea95c2cfc0e9bf037b5b6573cec322b
Joonyoung Park <mogun99@snu.ac.kr>
2023-05-27 19:00:20 +0900

    👷🏻‍♂️ Added music play and mic input into the pipeline

--- sketch.js
+++ sketch.js
@@ -2,23 +2,45 @@
  * sketch.js
  */
 
-const introLogoTime = 1000; // amount of time to display logo
-const introMessageTime = 1000; // amount of time to display intro message
-let isStartButtonPressed = false; // indicates if the start button is pressed or not
-let video; // captured video
+/* Constants and Global Variables */
+const introLogoTime = 2000; // amount of time to display logo
+const introMessageTime = 2000; // amount of time to display intro message
+const playMusicTime = 2000; // music will be played after this amount of time
 
+let video; // captured video
+let bgm; // background music
+let mic; // microphone input
+let audioOut; // sound sent from the microphone to the speaker
+
+let isStartButtonPressed = false; // indicates if the start button is pressed or not
+let isPlayButtonPressed = false; // indicates if the play button is pressed or not
+let isStopButtonPressed = false; // indicates if the stop button is pressed or not
+let gameStartedAt; // time when isStartButtonPressed is turned to true
+
+/* preload */
 function preload() {
   logoImg = loadImage("assets/logo_negative.png");
+  bgm = loadSound("sounds/sample2.mp3");
 }
 
+/* setup */
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // capture video from camera
   video = createCapture(VIDEO);
   video.hide();
+
+  // suspend audio context
+  getAudioContext().suspend();
+  bgm.play();
+
+  // get mic input
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
+/* draw */
 function draw() {
   background(0);
 
@@ -32,15 +54,64 @@ function draw() {
   } else if (!isStartButtonPressed) {
     // start button
     introStartButton();
-    if (
-      0.35 * width <= mouseX <= 0.65 * width &&
-      0.45 * height <= mouseY <= 0.55 * height &&
-      mouseIsPressed
-    ) {
+
+    if (isButtonClicked(width / 2, height / 2, 0.3 * width, 0.1 * height)) {
+      // when start button is pressed
       isStartButtonPressed = true;
+      gameStartedAt = ms;
     }
   } else {
-    // button is pressed -> start main
+    // start button is pressed -> start main
     showVideo(video);
+    console.log(mic.getLevel());
+
+    if (ms - gameStartedAt > playMusicTime && !isPlayButtonPressed) {
+      // when start button is pressed and playMusicTime has passes,
+      // display music play button
+      playMusicButton();
+
+      if (isButtonClicked(width / 2, height / 2, 0.3 * width, 0.1 * height)) {
+        // when play button is pressed
+        isPlayButtonPressed = true;
+      }
+    } else if (isPlayButtonPressed) {
+      // when play button is pressed
+      userStartAudio(); // enable audio context
+
+      stopMusicButton();
+      if (
+        isButtonClicked(0.8 * width, 0.8 * height, 0.3 * width, 0.1 * height)
+      ) {
+        // when stop button is pressed
+        isStopButtonPressed = true;
+      }
+
+      if (isStopButtonPressed) {
+        bgm.stop();
+      }
+
+      if (!bgm.isPlaying()) {
+        // when bgm is ended,
+        endingCredit(); // display ending credit
+      }
+    }
+  }
+}
+
+/* isButtonClicked
+  : returns true if the button whose center is (x, y) with
+    width of w and height of h is clicked
+ */
+function isButtonClicked(x, y, w, h) {
+  if (
+    x - w / 2 <= mouseX &&
+    mouseX <= x + w / 2 &&
+    y - h / 2 <= mouseY &&
+    mouseY <= y + h / 2 &&
+    mouseIsPressed
+  ) {
+    return true;
+  } else {
+    return false;
   }
 }
--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@
  * - Functions that consist main
  */
 
-// showVideo: displays a video captured by camera
+/* showVideo: displays a video captured by camera */
 function showVideo(video) {
   imageMode(CENTER);
   push(); // mirror mode
@@ -20,4 +20,47 @@ function showVideo(video) {
     (video.height * width) / video.width,
   );
   pop();
+}
+
+/* playMusicButton: displays a button to start a music */
+function playMusicButton() {
+  const message = "Press this button\nto play music"; // button message
+
+  // button
+  rectMode(CENTER);
+  fill(150);
+  rect(width / 2, height / 2, 0.3 * width, 0.1 * height);
+
+  // button text
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(25);
+  text(message, width / 2, height / 2, 0.3 * width, 0.1 * height);
+}
+
+function stopMusicButton() {
+  const message = "Press this button\nto stop music"; // button message
+
+  // button
+  rectMode(CENTER);
+  fill(150);
+  rect(0.8 * width, 0.8 * height, 0.3 * width, 0.1 * height);
+
+  // button text
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(25);
+  text(message, 0.8 * width, 0.8 * height, 0.3 * width, 0.1 * height);
+}
+
+function endingCredit() {
+  const message = "끝"; // message to display
+
+  background(0);
+  // text box
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  fill(255);
+  text(message, width / 2, height / 2, 0.8 * width, 0.5 * height);
 }
